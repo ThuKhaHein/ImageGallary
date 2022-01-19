54f4cd06c4e689686e5cf12d88df34332a7476a2
ThuKhaHein <kyawkyaw6716584@gmail.com>
2022-01-19 14:52:32 +0630

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,3 +3,36 @@ module.exports = {
     fallback: { process: require.resolve('process/browser') },
   },
 };
+
+// webpack.config.js
+const path = require( 'path' );
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+
+module.exports = {
+   context: __dirname,
+   entry: './src/index.js',
+   output: {
+      path: path.resolve( __dirname, 'dist' ),
+      filename: 'main.js',
+   },
+   module: {
+      rules: [
+         {
+            test: /\.js$/,
+            use: 'babel-loader',
+         },
+         {
+            test: /\.css$/,
+            exclude: /node_modules/,
+use: [
+   {
+      loader: MiniCssExtractPlugin.loader,
+   },
+   "css-loader"
+]
+         }
+      ]
+   },
+   plugins: [ new HtmlWebPackPlugin(), new MiniCssExtractPlugin() ]
+};
